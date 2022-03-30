03af384e5644d9847480adfa0bc63c722ba86f69
Paarth <paarth.jain2020@vitstudent.ac.in>
2022-03-31 03:16:16 +0530

    mender added

--- components/data/projectsData.js
+++ components/data/projectsData.js
@@ -1,3 +1,4 @@
+import Mender from '/public/mender.png';
 import SmartPDFS from '/public/smartpdfs.png';
 import RoboQuiz from '/public/roboquiz.png';
 import Vortex from '/public/vortex.png';
@@ -10,6 +11,14 @@ import Notes from '/public/notesapp.png';
 export const ProjectsData = [
   {
     id: 1,
+    img: Mender,
+    heading: "Mender Web",
+    description:
+      "",
+    link: "https://github.com/PAARTH2608/Mender"
+  },
+  {
+    id: 2,
     img: SmartPDFS,
     heading: "Smart PDF's",
     description:
@@ -17,7 +26,7 @@ export const ProjectsData = [
     link: "https://github.com/PAARTH2608/Smart-PDFs-Frontend"
   },
   {
-    id: 2,
+    id: 3,
     img: RoboQuiz,
     heading: "RoboVITics Quiz Portal",
     description:
@@ -25,7 +34,7 @@ export const ProjectsData = [
     link: "https://github.com/PAARTH2608/Robovitics-Quiz-Portal",
   },
   {
-    id: 3,
+    id: 4,
     img: Vortex,
     heading: "Vortex RoboVITics",
     description:
@@ -33,7 +42,7 @@ export const ProjectsData = [
     link: "https://github.com/PAARTH2608/Vortex-Website",
   },
   {
-    id: 4,
+    id: 5,
     img: Chat,
     heading: "Chat Application",
     description:
@@ -41,7 +50,7 @@ export const ProjectsData = [
     link: "https://github.com/PAARTH2608/Chat-App",
   },
   {
-    id: 5,
+    id: 6,
     img: PIC,
     heading: "Portfolio Website",
     description:
@@ -49,7 +58,7 @@ export const ProjectsData = [
     link: "https://github.com/PAARTH2608/Portfolio",
   },
   {
-    id: 6,
+    id: 7,
     img: FoodWeb,
     heading: "Foodiezz Web",
     description:
@@ -57,7 +66,7 @@ export const ProjectsData = [
     link: "https://github.com/PAARTH2608/Foodiezz_web",
   },
   {
-    id: 7,
+    id: 8,
     img: ForeCast,
     heading: "Fore Cast",
     description:
@@ -65,7 +74,7 @@ export const ProjectsData = [
     link: "https://github.com/PAARTH2608/fore_cast",
   },
   {
-    id: 8,
+    id: 9,
     img: Notes,
     heading: "Terminal Notes",
     description:
